65f310e8738b922b47dc529903ee69f5b5453667
LelinZheng <lezi.lelin@gmail.com>
2025-04-03 20:14:23 -0700

    Update Home Page with home page image

--- taskmaster-client/src/pages/Home.jsx
+++ taskmaster-client/src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
       <div className="text-center">
         <h1 className="mb-4">Welcome to TaskMaster ✅</h1>
         <p className="lead">Organize your tasks. Stay productive. Crush your goals.</p>
+        <img src="home_page_img.png"  alt="TaskMaster Illustration" className="img-fluid mx-auto d-block mt-4" style={{ maxWidth: '80%', height: 'auto' }}/>
       </div>
     </Layout>
   );
